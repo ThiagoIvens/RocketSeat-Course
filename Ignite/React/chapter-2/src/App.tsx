82c57fd604d2299e7295c7edf77380a97342738c
ThiagoIvens <thiagorech2121@gmail.com>
2022-03-17 11:54:23 -0300

    chapter 2 - module 4 (form show get and input values)

--- Ignite/React/chapter-2/src/App.tsx
+++ Ignite/React/chapter-2/src/App.tsx
@@ -1,13 +1,34 @@
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
+import { NewTransactionModal } from './components/NewTransactionModal';
+import Modal from 'react-modal';
+import { useState } from 'react';
 
+Modal.setAppElement('#root');
 
 export function App() {
+	const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsNewTransactionModalOpen(false);
+    }
+
 	return (
 		<>
-			<Header />
+			<Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
 			<Dashboard />
+
+			<NewTransactionModal 
+				isOpen={isNewTransactionModalOpen}
+				onRequestClose={handleCloseNewTransactionModal}
+			/>
+
 			<GlobalStyle />
 		</>
 	);
--- Ignite/React/chapter-2/src/styles/global.ts
+++ Ignite/React/chapter-2/src/styles/global.ts
@@ -14,6 +14,9 @@ export const GlobalStyle = createGlobalStyle`
 
         --background: #E5E5E5;
         --shape: #FFFFFF;
+
+        --input-backgroud: #e7e9ee;
+        --input-border: #d7d7d7;
     }
 
     * {
@@ -54,5 +57,42 @@ export const GlobalStyle = createGlobalStyle`
     [disabled] {
         opacity: 0.6;
         cursor: not-allowed;
+    }
+    
+    .react-modal-overlay {
+        background: rgba(0, 0, 0, 0.5);
+
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .react-modal-content {
+        width: 100%;
+        max-width: 576px;
+        background: var(--background);
+        padding: 3rem;
+        position: relative;
+        border-radius: 0.24rem;
+    }
+
+    .react-modal-close {
+        position: absolute;
+        right: 1.5rem;
+        top: 1.5rem;
+        border: 0;
+        background: transparent;
+        
+        transition: filter .2s;
+        
+        &:hover {
+            filter: brightness(0.8);
+        }
     }
 `
