c7962fbb9c016a93e94be7d645174dc09b43cd14
ThiagoIvens <thiago.rech@intergalaxy.com.br>
2022-05-06 02:36:30 -0300

    Server Side Rendering

--- Ignite/React/chapter-3/ignews/src/pages/index.tsx
+++ Ignite/React/chapter-3/ignews/src/pages/index.tsx
@@ -2,9 +2,18 @@ import Head from 'next/head'
 import { Header } from '../components/Header'
 import { SubscribeButton } from '../components/SubscribeButton'
 import styles from './home.module.scss'
+import { GetServerSideProps } from 'next';
+import { stripe } from '../services/stripe';
 
+interface HomeProps {
+	product: {
+		priceId: string;
+		amount: number;
+	}
+}
 
-export default function Home() {
+export default function Home({ product }: HomeProps) {
+
 	return (
 		<>
 			<Head>
@@ -18,13 +27,33 @@ export default function Home() {
 					the <span>React</span> world.</h1>
 					<p>
 						Get acess to all the publications <br />
-						<span>for $9.90 month</span>
+						<span>for { product.amount } month</span>
 					</p>
-					<SubscribeButton />
+					<SubscribeButton priceId={product.priceId} />
 				</section>
 
 				<img src="/images/avatar.svg" alt="Girl coding" />
 			</main>
 		</>
 	)
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+	const price = await stripe.prices.retrieve('price_1KwJEwFdo76ZCKAwviA6fCxj', {
+		expand: ['product']
+	});
+
+	const product = {
+		priceID: price.id,
+		amount: new Intl.NumberFormat('en-us', {
+			style: 'currency',
+			currency: 'USD'
+		}).format(price.unit_amount / 100),
+	};
+	
+	return {
+		props: {
+			product,
+		}
+	}
 }
