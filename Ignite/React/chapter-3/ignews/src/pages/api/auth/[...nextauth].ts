4ba7c6281088d262888a9bc963fa8e4c2a7c50e7
Thiago Rech <thiago.rech@JASSY.APOLLO>
2023-02-10 15:29:27 -0300

    subscription on stripe

--- Ignite/React/chapter-3/ignews/src/pages/api/auth/[...nextauth].ts
+++ Ignite/React/chapter-3/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,52 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
-export const authOptions = {
+
+import { fauna } from "../../../services/fauna"
+
+export default NextAuth({
 	providers: [
 		GithubProvider({
 			clientId: process.env.GITHUB_CLIENT_ID,
 			clientSecret: process.env.GITHUB_CLIENT_SECRET
 		}),
 	],
-}
-export default NextAuth(authOptions)
+	callbacks: {
+		async signIn({ user, account, profile }) {
+			const { email } = user
+
+			try {
+
+
+				await fauna.query(
+					q.If(
+						q.Not(
+							q.Exists(
+								q.Match(
+									q.Index('user_by_email'),
+									q.Casefold(user.email)
+								)
+							)
+						),
+						q.Create(
+							q.Collection('Users'),
+							{
+								data: { email }
+							}
+						),
+						q.Get(
+							q.Match(
+								q.Index('user_by_email'),
+								q.Casefold(user.email)
+							)
+						)
+					)
+				)
+				return true
+			} catch {
+				return false
+			}
+		}
+	}
+})
