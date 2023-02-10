66d07aa9c6b9bc1918bcaf2ff93d2470721bbd79
Thiago Rech <thiago.rech@JASSY.APOLLO>
2023-02-10 08:44:02 -0300

    next-auth

--- Ignite/React/chapter-3/ignews/src/components/SignInButton/index.tsx
+++ Ignite/React/chapter-3/ignews/src/components/SignInButton/index.tsx
@@ -1,19 +1,22 @@
 import { FaGithub } from 'react-icons/fa';
-import {FiX} from 'react-icons/fi';
+import { FiX } from 'react-icons/fi';
+import { useSession, signIn, signOut } from "next-auth/react"
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
-    
-    return isUserLoggedIn ? (
-        <button type="button" className={styles.signInButton}>
-            <FaGithub color="#04d361" />
-            Thiago Ivens Rech
+    const { data: session } = useSession();
+
+    console.log(session)
+
+    return session ? (
+        <button type="button" className={styles.signInButton} onClick={()=> signOut()}>
+            <FaGithub color="#04d361"  />
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon} />
         </button>
     ) : (
-        <button type="button" className={styles.signInButton}>
+        <button type="button" className={styles.signInButton} onClick={() => signIn('github')}>
             <FaGithub color="#eba417" />
             Sign in with Github
         </button>
